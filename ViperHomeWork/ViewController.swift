d036a7f976ec3028c635e89f4a2c726ddc68fb3a
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-10-31 13:44:16 +0300

    Initial Commit

---
+++ ViperHomeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViperHomeWork
+//
+//  Created by Эмиль Шалаумов on 31.10.2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
